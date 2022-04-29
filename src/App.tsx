ae46d507576c90e0c1a8ab85d97a30889b1698ed
JHSeo <qnfqnfqnf@gmail.com>
2022-04-29 20:51:40 +0900

    implement App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
 import {
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import Config from 'react-native-config';
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+
+import RootNavigation from './navigation/RootNavigation';
 import './i18n';
 
-console.log('config', Config.API_URL, Config.NODE_ENV);
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? '#000' : '#fff',
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <View style={backgroundStyle}>
-          <Text>App</Text>
-        </View>
-        <View style={backgroundStyle}>
-          <Text>{Config.NODE_ENV ?? 'not get'}</Text>
-        </View>
-        <View>
-          <Image source={require('./assets/images/sample.gif')} />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
