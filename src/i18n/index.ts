e96167f238efb53232dc9d0ec6e20b2a192404ca
JHSeo <qnfqnfqnf@gmail.com>
2022-04-30 18:40:42 +0900

    fix react-i18n config, type

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,33 +1,32 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import type { LanguageDetectorAsyncModule } from 'i18next';
 
 import en from './en/translation.json';
 import ko from './ko/translation.json';
 
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
+const languageDetector: LanguageDetectorAsyncModule = {
+  type: 'languageDetector',
+  async: true,
+  detect: cb => cb('en'),
+  init: () => {},
+  cacheUserLanguage: () => {},
+};
+
+export const resources = {
+  en: { translation: en },
+  ko: { translation: ko },
+} as const;
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  // .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  // .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
+  //
+  .use(languageDetector)
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     compatibilityJSON: 'v3',
     fallbackLng: 'en',
     debug: false,
-    resources: {
-      en,
-      ko,
-    },
+    resources,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
