1e7cdea0ceec45af16f3849bfc4b19fdd927903d
JHSeo <qnfqnfqnf@gmail.com>
2022-05-02 22:30:07 +0900

    update design-token

--- src/constants/design-token/fonts.ts
+++ src/constants/design-token/fonts.ts
@@ -1,22 +1,64 @@
-export const FONTS = {
+const poppins = {
+  /** 900 */
+  black: 'Poppins-Black',
+  /** 900 */
+  // blackItalic: 'Poppins-BlackItalic',
+  /** 800 */
+  extraBold: 'Poppins-ExtraBold',
+  /** 800 */
+  // extraBoldItalic: 'Poppins-ExtraBoldItalic',
   /** 700 */
   bold: 'Poppins-Bold',
   /** 700 */
-  boldItalic: 'Poppins-BoldItalic',
+  // boldItalic: 'Poppins-BoldItalic',
   /** 600 */
   semiBold: 'Poppins-SemiBold',
   /** 600 */
-  semiBoldItalic: 'Poppins-SemiBoldItalic',
+  // semiBoldItalic: 'Poppins-SemiBoldItalic',
   /** 500 */
   medium: 'Poppins-Medium',
   /** 500 */
-  mediumItalic: 'Poppins-MediumItalic',
+  // mediumItalic: 'Poppins-MediumItalic',
   /** 400 */
   regular: 'Poppins-Regular',
   /** 400 */
-  regularItalic: 'Poppins-Italic',
+  // regularItalic: 'Poppins-Italic',
   /** 300 */
   light: 'Poppins-Light',
   /** 300 */
-  lightItalic: 'Poppins-LightItalic',
+  // lightItalic: 'Poppins-LightItalic',
+  /** 200 */
+  extraLight: 'Poppins-ExtraLight',
+  /** 200 */
+  // extraLightItalic: 'Poppins-ExtraLightItalic',
+  /** 100 */
+  thin: 'Poppins-Thin',
+  /** 100 */
+  // thinItalic: 'Poppins-ThinItalic',
 } as const;
+
+const pretendard = {
+  /** 900 */
+  black: 'Pretendard-Black',
+  /** 800 */
+  extraBold: 'Pretendard-ExtraBold',
+  /** 700 */
+  bold: 'Pretendard-Bold',
+  /** 600 */
+  semiBold: 'Pretendard-SemiBold',
+  /** 500 */
+  medium: 'Pretendard-Medium',
+  /** 400 */
+  regular: 'Pretendard-Regular',
+  /** 300 */
+  light: 'Pretendard-Light',
+  /** 200 */
+  extraLight: 'Pretendard-ExtraLight',
+  /** 100 */
+  thin: 'Pretendard-Thin',
+};
+
+export const FONTS = {
+  poppins,
+  pretendard,
+};
