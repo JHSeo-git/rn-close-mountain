9a3fe3d8943424948a7d7bb24a08a1172da3d47c
JHSeo <qnfqnfqnf@gmail.com>
2022-05-03 22:20:55 +0900

    update Header with left, right icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,85 @@
 import { useNavigation } from '@react-navigation/native';
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, Platform } from 'react-native';
 import UIText from '../UIText';
 import * as viewStyles from '../../constants/global-styles/viewStyles';
-import { COLORS, FONTSIZES, SIZES, SPACE } from '../../constants/design-token';
+import { COLORS, SIZES, SPACE } from '../../constants/design-token';
+import { AllOrNone } from '../../utils/types/type-utils';
 
 import ChevronLeftSvg from '../../assets/icons/chevron-left.svg';
+import GearSvg from '../../assets/icons/gear.svg';
+import HamburgerMenuSvg from '../../assets/icons/hamburger-menu.svg';
+import SearchSvg from '../../assets/icons/search.svg';
+import CloseSvg from '../../assets/icons/close.svg';
 
-type HeaderProps = {
+type DefaultProps = {
   title?: string;
   hasGoback?: boolean;
 };
 
-const Header = ({ title, hasGoback }: HeaderProps) => {
+type RightButtonProps = {
+  rightIcon: 'gear' | 'hamberger-menu' | 'search' | 'close';
+  onRightIconPress: () => void;
+};
+
+type HeaderProps = DefaultProps & AllOrNone<RightButtonProps>;
+
+const Header = ({
+  title,
+  hasGoback = false,
+  rightIcon,
+  onRightIconPress,
+}: HeaderProps) => {
   const navigation = useNavigation();
+
+  const renderRightIcon = () => {
+    return (
+      <>
+        {rightIcon === 'gear' && (
+          <GearSvg width={24} height={24} color={COLORS.hiContrast} />
+        )}
+        {rightIcon === 'hamberger-menu' && (
+          <HamburgerMenuSvg width={24} height={24} color={COLORS.hiContrast} />
+        )}
+        {rightIcon === 'search' && (
+          <SearchSvg width={24} height={24} color={COLORS.hiContrast} />
+        )}
+        {rightIcon === 'close' && (
+          <CloseSvg width={24} height={24} color={COLORS.hiContrast} />
+        )}
+      </>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.leftBox}>
         {hasGoback && (
-          <Pressable onPress={() => navigation.goBack()}>
+          <Pressable
+            style={({ pressed }) => [
+              Platform.OS === 'ios' && pressed && { opacity: 0.6 },
+            ]}
+            onPress={() => navigation.goBack()}
+            android_ripple={{ color: COLORS.gray2 }}
+          >
             <ChevronLeftSvg width={32} height={32} color={COLORS.hiContrast} />
           </Pressable>
         )}
       </View>
       <View style={styles.centerBox}>
-        {title && <UIText style={styles.title}>{title}</UIText>}
+        {title && <UIText as="h2">{title}</UIText>}
       </View>
       <View style={styles.rightBox}>
-        <UIText> </UIText>
+        {rightIcon && (
+          <Pressable
+            style={({ pressed }) => [
+              Platform.OS === 'ios' && pressed && { opacity: 0.6 },
+            ]}
+            onPress={onRightIconPress}
+            android_ripple={{ color: COLORS.gray2 }}
+          >
+            {renderRightIcon}
+          </Pressable>
+        )}
       </View>
     </View>
   );
@@ -48,9 +100,6 @@ const styles = StyleSheet.create({
     zIndex: -1,
   },
   rightBox: {},
-  title: {
-    fontSize: FONTSIZES.xl,
-  },
 });
 
 export default Header;
