86f67a1d3f1745dc29b3fd32ad310f889e77d5de
JHSeo <qnfqnfqnf@gmail.com>
2022-05-03 22:21:19 +0900

    update textStyles with fontFamily

--- src/constants/global-styles/textStyles.ts
+++ src/constants/global-styles/textStyles.ts
@@ -1,47 +1,56 @@
-import { COLORS, FONTSIZES } from '../design-token';
+import { COLORS, FONTS, FONTSIZES } from '../design-token';
 import type { TextStyle } from 'react-native';
 
 export const h1: TextStyle = {
+  fontFamily: FONTS.poppins.bold,
   fontSize: FONTSIZES['4xl'],
   color: COLORS.hiContrast,
 };
 
 export const h2: TextStyle = {
+  fontFamily: FONTS.poppins.bold,
   fontSize: FONTSIZES['2xl'],
   color: COLORS.hiContrast,
 };
 
 export const h3: TextStyle = {
+  fontFamily: FONTS.poppins.bold,
   fontSize: FONTSIZES.xl,
   color: COLORS.hiContrast,
 };
 
 export const h4: TextStyle = {
+  fontFamily: FONTS.poppins.bold,
   fontSize: FONTSIZES.base,
   color: COLORS.hiContrast,
 };
 
 export const content: TextStyle = {
+  fontFamily: FONTS.poppins.regular,
   fontSize: FONTSIZES.base,
   color: COLORS.hiContrast,
 };
 
 export const p: TextStyle = {
+  fontFamily: FONTS.poppins.regular,
   fontSize: FONTSIZES.base,
   color: COLORS.hiContrast,
 };
 
 export const em: TextStyle = {
+  fontFamily: FONTS.poppins.regular,
   fontSize: FONTSIZES.base,
   color: COLORS.hiContrast,
 };
 
 export const strong: TextStyle = {
+  fontFamily: FONTS.poppins.bold,
   fontSize: FONTSIZES.base,
   color: COLORS.hiContrast,
 };
 
 export const small: TextStyle = {
+  fontFamily: FONTS.poppins.regular,
   fontSize: FONTSIZES.sm,
   color: COLORS.hiContrast,
 };
