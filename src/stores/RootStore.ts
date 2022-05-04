3ff27534725c598f7bba971377891e99fee7f977
JHSeo <qnfqnfqnf@gmail.com>
2022-05-05 01:50:04 +0900

    add SignInStore

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,14 +1,18 @@
 import AuthStore from './AuthStore';
+import SignInStore from './SignInStore';
 import SampleStore from './__sample__/SampleStore';
 
 class RootStore {
   SampleStore: SampleStore;
   AuthStore: AuthStore;
+  SignInStore: SignInStore;
+
   constructor() {
     // sample
     this.SampleStore = new SampleStore();
 
     this.AuthStore = new AuthStore();
+    this.SignInStore = new SignInStore();
   }
 }
 
---
+++ src/stores/SignInStore.ts
@@ -0,0 +1,31 @@
+import { action, makeObservable, observable } from 'mobx';
+
+class SignInStore {
+  email: string | undefined;
+  password: string | undefined;
+
+  constructor() {
+    makeObservable(this, {
+      email: observable,
+      password: observable,
+      setEmail: action,
+      setPassword: action,
+      reset: action,
+    });
+  }
+
+  setEmail(email: string | undefined) {
+    this.email = email;
+  }
+
+  setPassword(password: string | undefined) {
+    this.password = password;
+  }
+
+  reset() {
+    this.email = undefined;
+    this.password = undefined;
+  }
+}
+
+export default SignInStore;
