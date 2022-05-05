62ae48d266bb99f40c13902935dbab004b29a798
JHSeo <qnfqnfqnf@gmail.com>
2022-05-05 23:21:44 +0900

    add Loader for full screen loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,37 @@
+import { observer } from 'mobx-react-lite';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Portal } from 'react-native-paper';
+import { useStore } from '../../contexts/StoreContext';
+import { COLORS } from '../../constants/design-token';
+import * as viewStyles from '../../constants/global-styles/viewStyles';
+
+const Loader = observer(() => {
+  const { LoaderStore } = useStore();
+
+  if (!LoaderStore.loading) {
+    return null;
+  }
+
+  return (
+    <Portal>
+      <View style={styles.container}>
+        <ActivityIndicator
+          animating={LoaderStore.loading}
+          size="large"
+          color={COLORS.primary}
+        />
+      </View>
+    </Portal>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    ...viewStyles.center,
+    flex: 1,
+    zIndex: 9999,
+    backgroundColor: COLORS.grayA8,
+  },
+});
+
+export default Loader;
