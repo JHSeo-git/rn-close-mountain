940722172425eca3f05f88729684cccfefa5c3b2
JHSeo <qnfqnfqnf@gmail.com>
2022-05-06 23:47:25 +0900

    add signIn action

--- src/stores/EmailSignInStore.ts
+++ src/stores/EmailSignInStore.ts
@@ -1,15 +1,23 @@
-import { action, makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable, override } from 'mobx';
+import emailSignIn from '../utils/api/auth/emailSignIn';
+import BaseStore from './base/BaseStore';
+import RootStore from './RootStore';
 
-class EmailSignInStore {
+class EmailSignInStore extends BaseStore {
   email: string | undefined;
   password: string | undefined;
 
-  constructor() {
+  constructor(root: RootStore) {
+    super(root);
     makeObservable(this, {
+      loading: override,
+      error: override,
       email: observable,
       password: observable,
       setEmail: action,
       setPassword: action,
+      setFetchData: action,
+      signIn: action,
       reset: action,
     });
   }
@@ -22,7 +30,37 @@ class EmailSignInStore {
     this.password = password;
   };
 
+  setFetchData = (email: string, password: string) => {
+    this.email = email;
+    this.password = password;
+  };
+
+  signIn = async () => {
+    this.loading = true;
+
+    try {
+      if (!this.email || !this.password) {
+        throw new Error('Email or password is not set');
+      }
+
+      const result = await emailSignIn({
+        email: this.email,
+        password: this.password,
+      });
+
+      return result;
+    } catch (e: unknown) {
+      throw e;
+    } finally {
+      this.loading = false;
+    }
+  };
+
+  signOut = async () => {};
+
   reset = () => {
+    this.loading = false;
+    this.error = null;
     this.email = undefined;
     this.password = undefined;
   };
