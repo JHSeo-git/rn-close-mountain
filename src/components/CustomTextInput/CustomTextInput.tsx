641828c566c406b0c070f1c02025b065d6f8285d
JHSeo <qnfqnfqnf@gmail.com>
2022-05-06 23:44:07 +0900

    add forwardedRef for using focus

--- src/components/CustomTextInput/CustomTextInput.tsx
+++ src/components/CustomTextInput/CustomTextInput.tsx
@@ -1,34 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { forwardRef } from 'react';
+import { StyleSheet, TextInput as RNTextInput } from 'react-native';
 import { HelperText, TextInput } from 'react-native-paper';
 import { COLORS } from '../../constants/design-token';
 
 type CustomTextInputProps = {
   errorText?: string;
-} & React.ComponentProps<typeof TextInput>;
+} & React.ComponentPropsWithRef<typeof TextInput>;
 
-const CustomTextInput = ({
-  errorText,
-  mode = 'outlined',
-  outlineColor = COLORS.gray6,
-  autoCapitalize = 'none',
-  style,
-  ...props
-}: CustomTextInputProps) => {
-  return (
-    <>
-      <TextInput
-        mode={mode}
-        style={[styles.defaultStyle, style]}
-        outlineColor={outlineColor}
-        autoCapitalize={autoCapitalize}
-        {...props}
-      />
-      <HelperText type="error" visible={!!errorText}>
-        {errorText}
-      </HelperText>
-    </>
-  );
-};
+const CustomTextInput = forwardRef<RNTextInput, CustomTextInputProps>(
+  (
+    {
+      errorText,
+      mode = 'outlined',
+      outlineColor = COLORS.gray6,
+      autoCapitalize = 'none',
+      style,
+      ...props
+    },
+    forwardedRef,
+  ) => {
+    return (
+      <>
+        <TextInput
+          ref={forwardedRef as any}
+          mode={mode}
+          style={[styles.defaultStyle, style]}
+          outlineColor={outlineColor}
+          autoCapitalize={autoCapitalize}
+          {...props}
+        />
+        {errorText && (
+          <HelperText type="error" visible={!!errorText}>
+            {errorText}
+          </HelperText>
+        )}
+      </>
+    );
+  },
+);
+
+CustomTextInput.displayName = 'CustomTextInput';
+
 const styles = StyleSheet.create({
   defaultStyle: {},
 });
