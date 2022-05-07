b69e079cebdc211504eb9ae5bfd720bae98cacdb
JHSeo <qnfqnfqnf@gmail.com>
2022-05-07 22:48:42 +0900

    add axios client with types

---
+++ src/api/types.ts
@@ -0,0 +1,11 @@
+type ErrorName = 'ValidationError';
+
+export type AxiosErrorResponse = {
+  data: object | null;
+  error: {
+    status: number;
+    name: string;
+    message: string;
+    details: object;
+  };
+};
