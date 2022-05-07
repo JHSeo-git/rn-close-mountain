c4ef2479d65eaed9157552c48ec0a1ce85ba0325
JHSeo <qnfqnfqnf@gmail.com>
2022-05-07 23:07:39 +0900

    add simple text for check login authentication

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -5,20 +5,25 @@ import Header from '../../components/Header';
 import UIText from '../../components/UIText';
 import * as viewStyles from '../../constants/global-styles/viewStyles';
 import type { HomeStackScreenProps } from '../types';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../../contexts/StoreContext';
 
 type HomeScreenProps = {} & HomeStackScreenProps<'Home'>;
 
-const HomeScreen = ({}: HomeScreenProps) => {
+const HomeScreen = observer(({}: HomeScreenProps) => {
   const { t } = useTranslation();
+  const { authStore } = useStore();
 
   return (
     <SafeAreaView style={viewStyles.flex_1_bg_white}>
       <Header title={t('common.home')} />
       <View style={viewStyles.flex_1_padding_20}>
-        <UIText>Home</UIText>
+        <UIText>
+          {authStore.isAuthenticated ? 'Welcome My Lord' : 'Not Logged in'}
+        </UIText>
       </View>
     </SafeAreaView>
   );
-};
+});
 
 export default HomeScreen;
