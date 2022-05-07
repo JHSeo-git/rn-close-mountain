f059b11467f1f2386e14a737c0704d0c6dae6c89
JHSeo <qnfqnfqnf@gmail.com>
2022-05-07 22:50:21 +0900

    add AppError
    - Error class for error handling in app

---
+++ src/utils/error/AppError.ts
@@ -0,0 +1,29 @@
+type AppErrorLabel = 'VALIDATION' | 'API' | 'UNKNOWN';
+
+type AppConstructorParams = {
+  message: string;
+  name: string;
+  label: AppErrorLabel;
+  status?: number;
+  stack?: string;
+};
+
+class AppError extends Error {
+  label: AppErrorLabel;
+  status: number | undefined;
+  constructor({
+    message,
+    name,
+    label = 'UNKNOWN',
+    status,
+    stack,
+  }: AppConstructorParams) {
+    super(message);
+    this.name = name;
+    this.label = label;
+    this.status = status;
+    this.stack = stack;
+  }
+}
+
+export default AppError;
