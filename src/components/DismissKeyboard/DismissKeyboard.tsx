40c050c84ef71ff5868d6beacb2e555565efde16
JHSeo <qnfqnfqnf@gmail.com>
2022-05-12 23:44:41 +0900

    fix style to contentContainerStyle in KeyboardAwareScrollView

--- src/components/DismissKeyboard/DismissKeyboard.tsx
+++ src/components/DismissKeyboard/DismissKeyboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 type DismissKeyboardProps = {
@@ -10,7 +10,7 @@ const DismissKeyboard = ({ children }: DismissKeyboardProps) => (
   <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
     <KeyboardAwareScrollView
       keyboardShouldPersistTaps="handled"
-      style={{ flex: 1 }}
+      contentContainerStyle={{ flex: 1 }}
     >
       {children}
     </KeyboardAwareScrollView>
