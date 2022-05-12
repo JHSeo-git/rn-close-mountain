e495fa9dba822006b91dedda06857b6ee1b006cb
JHSeo <qnfqnfqnf@gmail.com>
2022-05-12 23:43:52 +0900

    update BiometricAuth

--- src/components/BiometricAuth/BiometricAuth.tsx
+++ src/components/BiometricAuth/BiometricAuth.tsx
@@ -6,9 +6,7 @@ import { Platform, StyleSheet, View } from 'react-native';
 import { COLORS, SPACE } from '../../constants/design-token';
 import { useStore } from '../../contexts/StoreContext';
 import { MainTabScreenProps } from '../../screens/types';
-import UIBottomSheetModal, {
-  UIBottomSheetModalRef,
-} from '../UIBottomSheetModal';
+import UIBottomSheetModal, { UIBottomSheetModalRef } from '../UIBottomSheetModal';
 import UIIcon from '../UIIcon';
 import UIText from '../UIText';
 
@@ -16,30 +14,33 @@ const BiometricAuth = observer(() => {
   const bottomSheetRef = useRef<UIBottomSheetModalRef>(null);
   const { t } = useTranslation();
   const { appSettingStore, biometricStore, authStore } = useStore();
-  const navigation =
-    useNavigation<MainTabScreenProps<'Profile'>['navigation']>();
+  const navigation = useNavigation<MainTabScreenProps<'Profile'>['navigation']>();
 
-  const title =
-    Platform.OS === 'android' ? t('setting.touchId') : t('setting.faceId');
+  const title = Platform.OS === 'android' ? t('setting.touchId') : t('setting.faceId');
 
   useEffect(() => {
     const process = async () => {
+      // 로그인 되어 있지 않으면 체크하지 않음
       if (!authStore.isAuthenticated) {
         return;
       }
+      // 생체 인증 설정되어 있지 않으면 체크하지 않음
       if (!appSettingStore.isUseBiometric) {
         return;
       }
 
       const isValidReady = await biometricStore.checkBiometric();
       if (!isValidReady) {
+        // 생체 인증 준비가 되어 있지 않으면 체크하지 않고
+        // 생체 인증 사용 토글 false
+        await appSettingStore.setUseBiometric(false);
         return;
       }
 
       const result = await biometricStore.authenticate();
 
       if (!result.success) {
-        // 1. 생체인증 사용 false
+        // 1. 생체인증 사용 토글 false
         // 2. 로그아웃
         bottomSheetRef.current?.present();
         await appSettingStore.setUseBiometric(false);
