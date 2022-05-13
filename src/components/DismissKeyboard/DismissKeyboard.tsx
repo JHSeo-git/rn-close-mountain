5d24d9c5cd24436fe22b3bb67003b65bb74bbbd4
JHSeo <qnfqnfqnf@gmail.com>
2022-05-13 23:35:27 +0900

    add handleKeyboardWill event

--- src/components/DismissKeyboard/DismissKeyboard.tsx
+++ src/components/DismissKeyboard/DismissKeyboard.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { isKeyboardFrameT } from './DismissKeyboard.types';
 
 type DismissKeyboardProps = {
   children: React.ReactNode;
+  setKeyboardHeight?: (height: number) => void;
 };
 
-const DismissKeyboard = ({ children }: DismissKeyboardProps) => (
-  <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
-    <KeyboardAwareScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
-    >
-      {children}
-    </KeyboardAwareScrollView>
-  </TouchableWithoutFeedback>
-);
+const DismissKeyboard = ({ children, setKeyboardHeight }: DismissKeyboardProps) => {
+  const handleKeyboardWillShowHide = (frame: Object, show: boolean) => {
+    if (isKeyboardFrameT(frame)) {
+      if (setKeyboardHeight) {
+        setKeyboardHeight(show ? frame.endCoordinates.height : 0);
+      }
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+      <KeyboardAwareScrollView
+        keyboardShouldPersistTaps="handled"
+        onKeyboardWillShow={frame => handleKeyboardWillShowHide(frame, true)}
+        onKeyboardWillHide={frame => handleKeyboardWillShowHide(frame, false)}
+        contentContainerStyle={{ flex: 1 }}
+      >
+        {children}
+      </KeyboardAwareScrollView>
+    </TouchableWithoutFeedback>
+  );
+};
 
 export default DismissKeyboard;
