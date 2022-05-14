15697522f5247a762657124f9486510dad984230
JHSeo <qnfqnfqnf@gmail.com>
2022-05-14 21:20:50 +0900

    add UIScreenTitleView
    - common title styled wrapper view

---
+++ src/components/UIScreenTitleView/UIScreenTitleView.tsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet } from 'react-native';
+import { SPACE } from '../../constants/design-token';
+import UIText from '../UIText';
+
+type UIScreenTitleViewProps = {
+  title: string;
+  subTitle: string;
+  children?: React.ReactNode;
+};
+
+const UIScreenTitleView = ({ title, subTitle, children }: UIScreenTitleViewProps) => {
+  return (
+    <View style={styles.titleBox}>
+      <UIText as="h1" style={styles.textStyle}>
+        {title}
+      </UIText>
+      <UIText as="content" style={[styles.textStyle, styles.mt]}>
+        {subTitle}
+      </UIText>
+      {children && <View style={styles.mt}>{children}</View>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleBox: {},
+  textStyle: {
+    textAlign: 'center',
+  },
+  mt: {
+    marginTop: SPACE.$2,
+  },
+});
+
+export default UIScreenTitleView;
