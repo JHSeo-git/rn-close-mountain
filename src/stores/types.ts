554d3d4e5833fe142d6693ca37e8cf1d1228ca74
JHSeo <qnfqnfqnf@gmail.com>
2022-05-17 00:05:24 +0900

    update types in store

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -1,5 +1,8 @@
 import { Provider } from '../api/auth/types';
 
+export type BiometricType = 'faceId' | 'touchId' | 'iris' | 'unknown';
+export type SnackbarType = 'success' | 'error' | 'info' | 'default';
+
 export type SessionInfo = {
   token: string;
   provider: Provider;
