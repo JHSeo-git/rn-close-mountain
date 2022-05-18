219334b36c9b5cfed08371f71c29dfd54b4ea569
JHSeo <qnfqnfqnf@gmail.com>
2022-05-18 23:44:00 +0900

    update verification code apis
    - for common target(email, mobile, otp, ...)

--- src/api/auth/checkVerificationCode.ts
+++ src/api/auth/checkVerificationCode.ts
@@ -2,22 +2,23 @@ import client from '../client';
 import type { NoContent204Response, VerificationProvider, VerificationUseType } from './types';
 
 export type CheckVerificationCodeRequest = {
-  email: string;
+  targetForSendCode: string;
   verificationUseType: VerificationUseType;
   code: string;
   verificationProvider: VerificationProvider;
 };
 
 export default async function checkVerificationCode({
-  email,
+  targetForSendCode,
   verificationUseType,
   code,
   verificationProvider,
 }: CheckVerificationCodeRequest) {
   const { data } = await client.post<NoContent204Response>('/api/auth/verify/check-code', {
-    email,
-    type: verificationUseType,
+    targetForSendCode,
+    verificationUseType,
     code,
+    verificationProvider,
   });
 
   return data;
--- src/api/auth/sendVerificationCode.ts
+++ src/api/auth/sendVerificationCode.ts
@@ -2,19 +2,20 @@ import client from '../client';
 import type { NoContent204Response, VerificationProvider, VerificationUseType } from './types';
 
 export type SendVerificationCodeRequest = {
-  email: string;
+  targetForSendCode: string;
   verificationProvider: VerificationProvider;
   verificationUseType: VerificationUseType;
 };
 
 export default async function sendCode({
-  email,
+  targetForSendCode,
   verificationProvider,
   verificationUseType,
 }: SendVerificationCodeRequest) {
   const { data } = await client.post<NoContent204Response>('/api/auth/verify/send-code', {
-    email,
-    type: verificationUseType,
+    targetForSendCode,
+    verificationProvider,
+    verificationUseType,
   });
 
   return data;
