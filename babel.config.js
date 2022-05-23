eaf024199800327d41371c9351c077703478517a
JHSeo <qnfqnfqnf@gmail.com>
2022-05-23 22:43:48 +0900

    chore babel config and add library
    - mv babel.config.js to package.json.babel for @babel/plugin-proposal-class-properties plugin
    - add qs library for using queryparams

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,9 @@
 module.exports = {
-  presets: [
-    [
-      'module:metro-react-native-babel-preset',
-      { useTransformReactJSXExpermiental: true },
-    ],
-  ],
+  presets: [['module:metro-react-native-babel-preset', { useTransformReactJSXExpermiental: true }]],
   plugins: [
     ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    // ['@babel/plugin-proposal-class-properties', { loose: true }], // move to package.json
+    'react-native-paper/babel',
     'react-native-reanimated/plugin', // Reanimated plugin has to be listed last.
   ],
-  env: {
-    production: {
-      // To get smaller bundle size by excluding modules you don't use,
-      // you can use our optional babel plugin.
-      plugins: ['react-native-paper/babel'],
-    },
-  },
 };
